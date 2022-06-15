9fa8903631f00772c17c1227b28f6dceee144970
Yuval Nachon <yuval.nachon@sentry.io>
2022-06-14 21:53:39 -0700

    New version with more errors

--- index.js
+++ index.js
@@ -1,7 +1,30 @@
 function load() {
-    
+    Sentry.init({
+        dsn: "https://fa13e8d770fc4d61b9de581d678b2deb@o87286.ingest.sentry.io/6503225",
+        release: "1.1",
+        integrations: function(integrations) {
+            return integrations.filter(function(integration) {
+                return integration.name !== "InboundFilters" && integration.name !== "Dedupe";
+            });
+        }
+      });
+
+
+    var box = document.getElementById("box1");
+    box.addEventListener("click", onBoxClick);
 };
 function onBoxClick(e) {
-    
+    var box = e.target;   
+    if (box.style.backgroundColor == "blue") {
+        box.style.backgroundColor = "white";
+    }
+    else {
+        box.style.backgroundColor = "blue";
+    }
+};
+
+function generateAnError() {
+    var obj = {};
+    obj.david();  
 };
 window.onload = load;
