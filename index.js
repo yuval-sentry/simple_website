8e2abcfb01232b662afc567395756e7a6027d8eb
yuval-sentry <89414234+yuval-sentry@users.noreply.github.com>
2022-06-17 08:50:02 -0700

    update index.js

--- index.js
+++ index.js
@@ -2,15 +2,15 @@ function load() {
     Sentry.init({
         dsn: "https://fa13e8d770fc4d61b9de581d678b2deb@o87286.ingest.sentry.io/6503225",
         release: "7.7",
-        integrations: [new Sentry.Integrations.BrowserTracing()],
-        debug: true,
+        //integrations: [new Sentry.Integrations.BrowserTracing()],
+        //debug: true,
         tracesSampleRate: 1.0,
-        // integrations: function(integrations) {
-        //     return integrations.filter(function(integration) {
-        //         return integration.name !== "InboundFilters" && integration.name !== "Dedupe";
-        //     });
-        // }
-      });
+        integrations: function(integrations) {
+            return integrations.filter(function(integration) {
+                return integration.name !== "InboundFilters" && integration.name !== "Dedupe";
+            });
+        }
+      });  
 
 
     var box = document.getElementById("box1");
@@ -22,7 +22,7 @@ function onBoxClick(e) {
         box.style.backgroundColor = "white";
     }
     else {
-        ox.style.backgroundColor = "blue";
+        box.style.backgroundColor = "blue";
     }
 };
 
